4b071dd169d1d0699f640d218bfdab4c02900d82
Ludovicmanga <ludovic.mangaj@gmail.com>
2023-10-04 17:10:11 +0200

    skeleton set

--- client/components/StyledSkeleton/StyledSkeleton.tsx
+++ client/components/StyledSkeleton/StyledSkeleton.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
+import styles from './StyledSkeleton.module.scss';
 
 type Props = {};
 
 const StyledSkeleton = (props: Props) => {
-  const numberOfSkeletons = 10; // Number of Skeleton elements to render
+  const numberOfSkeletons = 4; // Number of Skeleton elements to render
 
   return (
-    <Stack spacing={1}>
+    <Stack className={styles.skeletonContainer} spacing={1}>
       {Array.from({ length: numberOfSkeletons }).map((_, index) => (
-        <Skeleton key={index} variant="rounded" width="100%" height={60} />
+        <Skeleton key={index}  width="100%" height={100} />
       ))}
     </Stack>
   );
 };
 
 export default StyledSkeleton;
+
